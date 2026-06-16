d9cbd24bc8a0606e24f54bbd1726f62190609970
Harshith1110-P <2400031436@kluniversity.in>
2026-06-16 19:35:16 +0530

    Update CORS configuration to support Netlify deploys

--- server/server.js
+++ server/server.js
@@ -13,7 +13,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin || origin.startsWith('http://localhost') || origin.includes('netlify.app')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
